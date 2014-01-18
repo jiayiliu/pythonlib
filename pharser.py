f2cb1aad1808a68a9da202c80c9dd66a4a288699
Jiayi Liu <liujiayi.liu@gmail.com>
2014-01-18 14:13:03 +0100

    finish note class

--- pharser.py
+++ pharser.py
@@ -1,61 +1,75 @@
 #coding=utf-8
-''' python library processing code
+## @package pharser
+#  python library processing code
+#
+#  JSON data structure
+# 
+#  - object - 
+#  {
+#  "Author": xxx,
+#  "Date_create": xxx,
+#  "Date_change": xxx,
+#  "tags": [xxx,xxx,xxx],
+#  "ctags": [xxx,xxx,xxx],
+#  "content": xxx
+#  }
 
-JSON data structure
-
-- object - 
-{
-"Author": xxx,
-"Date_create": xxx,
-"Date_change": xxx,
-"tags": [xxx,xxx,xxx],
-"ctags": [xxx,xxx,xxx],
-"content": xxx
-}
-'''
 
 from os.path import isfile
 from sys import exit
 import re
 
+## Newline symbol for Linux system
 NEWLINE = '\n'
 
-def warning(line):
-    """print warning context in red
-    
-    Args:
-    line (str): content want to print
-    """
+## \fn void warning(message)
+#  \brief Create warning message in red
+#  \param message a string of warning message
+def warning(message):
+    ## Define warning color
     cWARNING = '\033[31m'
+    ## Define Normal color
     ENDC = '\033[0m'
-    print cWARNING+line+ENDC
-    
+    print cWARNING+message+ENDC
+
+##
+#  \brief Class for markdown file note
+#  \details This class build to load the file into program and support multiple output formats
+#  \author Jiayi Liu
+#  \date 2014-01-17
+#
 class Note:
-    """ Python class to store the content of one markdown file.
-
-    """
+    ##
+    # \fn Note __init__(filename)
+    # \brief Initialization code
+    # \param filename name string of a file to load-in
     def __init__(self,filename):
-        """An initialization code
-
-        Args:
-           filename (str): the name of a file to load-in
-        """
         try:
-            #### load file content
+            # !!! Exam file existency !!! #
             if not isfile(filename):
                 raise Exception("!! Fail to open %s !!"%filename)
-            #### initial empty content
+            # !!! initial empty content !!! #
+            ## Filename of the Note 
             self.filename = filename
+            ## Author of the Note 
             self.author = "EMPTY"
+            ## Create Date of the Note
+            self.dateCreate = "EMPTY"
+            ## Create Date of the Note
             self.dateChange = "EMPTY"
-            self.date = "EMPTY"
+            ## Tags for the Note
             self.tags = []
+            ## Tags for the Note (Chinese)
             self.ctags = []
+            ## Main content of the Note
             self.content = ''
-            
+            # !!! Load file content !!! #
             with open(filename,'r') as f:
                 fulltext = f.readlines()
-            ### phrase content 
+            # !!! empty file case !!! #
+            if len(fulltext) == 0:
+                raise Exception("!! File %s is empty"%filename)
+            # !!! phrasing content !!! #
             if fulltext[0].strip(NEWLINE) != "<!--":
                 raise Exception("!! Fail to recognize header as \n %s"
                                 %fulltext[0].strip(NEWLINE))
@@ -64,7 +78,7 @@ class Note:
                 if (line.strip(NEWLINE) == "-->"):
                     finishFlag = ii
                     break
-                ## Start pumping header
+                # !!! Start pumping header !!! #
                 currentName = ''
                 if line[0] == '+':  # identify header
                     ia = line.find(':')
@@ -75,17 +89,17 @@ class Note:
                     ib = ib if ib != -1 else 100000
                     if ib > ia: # choose the smallest
                         currentName = line[1:ia]
-                        self.populateHeaderObj(currentName,line[ia+1:])
+                        self.__populateHeaderObj(currentName,line[ia+1:])
                     else:
                         currentName = line[1:ib]
-                        self.populateHeaderObj(currentName,line[ib+3:])
+                        self.__populateHeaderObj(currentName,line[ib+3:])
                 elif currentName != '': # continue previous header
-                    self.populateHeaderObj(currentName,line)
+                    self.__populateHeaderObj(currentName,line)
                 else:  # unrecognized case
-                    pass
+                    warning("unknown case:: %s"%line)
             if not finishFlag:
                 raise Exception("!! Unfinished header !!")
-            ## load content
+            # !!! insert main body !!! #
             if len(fulltext) == ii+2:
                 raise Exception("!! Empty Content !!")
             self.content = "".join(fulltext[ii+2:])
@@ -93,9 +107,12 @@ class Note:
             warning(inst.args[0])
             exit(1)
 
-    def populateHeaderObj(self,name,content):
-        """Populate the header content into Note class
-        """
+    ##
+    # \fn populateHeaderObj(name,content)
+    # \brief Populate the header content into Note class
+    # \param name the keyword
+    # \param content string contains all keywords
+    def __populateHeaderObj(self,name,content):
         content = content.strip() # remove leading and trailing spaces
         if name.lower() == "author": 
             self.author = content
@@ -104,26 +121,33 @@ class Note:
         if name.lower() == "date changed":
             self.dateChange = content
         if name.lower() == "tags":
-            self.populateTagArray(content)
+            self.__populateTagArray(content)
         if name.lower() == "标签":
-            self.populateTagArrayS(content)
-
-    def populateTagArray(self,content):
-        """Populate the header tag array, work only for or ";"
-        """
+            self.__populateTagArrayS(content)
+        return
+    
+    ##
+    # \fn populateTagArray(content)
+    # \brief Populate the header tags array, work only for or ";"
+    # \param content a ;-seperated list of tags
+    def __populateTagArray(self,content):
         self.tags = re.split(';+', content)
         self.tags = [x.strip().capitalize() for x in self.tags if x]
-
-    def populateTagArrayS(self,content):
-        """Populate the header tag array, work only for "；" or ";"
-        Mainly for Chinese 标签
-        """
+        return
+    
+    ##
+    # \fn populateTagArrayS(content)
+    # \brief Populate the header ctags array, work only for or ";" and "；" 中文标签
+    # \param content a ;-seperated list of tags
+    #  Populate the header tag array, work only for  or ";"
+    def __populateTagArrayS(self,content):
         self.ctags = re.split(';+|；+', content)
         self.ctags = [x.strip().capitalize() for x in self.ctags if x]
-
+        return 
+    ##
+    # \fn str returnHeader()
+    # \brief return header information in one string
     def returnHeader(self):
-        """Return header informaiton as string
-        """
         str = "<!--\n" + \
             "+Author: %s\n"%self.author + \
             "+Date Created: %s\n"%self.dateCreate + \
@@ -133,33 +157,28 @@ class Note:
             "-->\n"
         return str
 
+    ##
+    # \fn writeFile(filename)
+    # \brief write Note into file
+    # \param filename name of the file to write in (need to be a new file)
     def writeFile(self,filename):
-        """print to file
-        """
         try:
             if isfile(filename):
                 raise Exception("!! File (%s)Exist; Stop to overwrite !!"%filename)
             with open(filename,'w') as f:
-                f.write(self.returnHeader()+content
+                f.write(self.returnHeader()+content)
         except Exception as inst:
             warning(inst.args[0])
             exit(1)
+        return
         
-def loadfile(filename):
-    '''convert file into JSON structure
-    '''
-    pass
-    
-def insertDB():
-    '''insert data into database
-    '''
-    pass
-
+##
+# \fn main()
+# for testing
 def main():
-    ''' main code 
-    '''
     a = Note("rules.md")
     print a.returnHeader()+a.content
-    
+
 if __name__=="__main__":
     main()
+
